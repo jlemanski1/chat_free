53b6cf955243b94bc04cd69cedacf02719a89fe2
Jonathan Lemanski <jlemanski1@gmail.com>
2019-11-25 15:19:14 -0700

    Created ConvoScreen and added to routes

---
+++ lib/screens/convo_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class ConvoScreen extends StatefulWidget {
+  static const String id = 'registration_screen'; // ID of this particular screen
+
+  @override
+  _ConvoScreenState createState() => _ConvoScreenState();
+}
+
+class _ConvoScreenState extends State<ConvoScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
